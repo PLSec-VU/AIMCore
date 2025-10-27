1d8a066982d67aac8dd5941d3d6359fa9e63921c
zfnmxt <zfnmxt@zfnmxt.com>
2025-10-26 21:54:47 -0400

    Forgot to add this.

---
+++ src/Access.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+module Access where
+
+import Clash.Prelude hiding (Ordering (..), Word, def, init, lift)
+import Data.Functor.Identity
+import Data.Maybe
+import Prelude hiding (Ordering (..), Word, init, lines, not, undefined, (&&), (||))
+
+class (Monad f) => Access f where
+  unAccess :: f a -> a
+  fromPublic :: f a -> Maybe a
+  fromSecret :: f a -> Maybe a
+
+isPublic :: (Access f) => f a -> Bool
+isPublic = isJust . fromPublic
+
+isSecret :: (Access f) => f a -> Bool
+isSecret = isJust . fromSecret
+
+-- | No secrets here, buddy: unwrap a word. If it's public, we gucci. If it's
+-- private, die.
+noSecrets :: (MonadFail m, Access f) => f a -> (a -> m b) -> m b
+noSecrets w m =
+  maybe (fail "die") m $ fromPublic w
+
+data PubSec a
+  = Public a
+  | Secret a
+  deriving (Functor, Show, Eq, Generic, NFDataX)
+
+instance Access PubSec where
+  unAccess (Public a) = a
+  unAccess (Secret a) = a
+
+  fromPublic (Public a) = pure a
+  fromPublic _ = empty
+
+  fromSecret (Secret a) = pure a
+  fromSecret _ = empty
+
+instance Applicative PubSec where
+  pure = Public
+  f <*> x
+    | isSecret f || isSecret x = Secret $ unAccess f $ unAccess x
+    | otherwise = Public $ unAccess f $ unAccess x
+
+instance Monad PubSec where
+  (Public x) >>= f = f x
+  (Secret x) >>= f = Secret $ unAccess $ f x
+
+instance Access Identity where
+  unAccess = runIdentity
+  fromPublic = pure . unAccess
+  fromSecret = const empty
