5d8d04efcb929c585d66c7a9aa7e89c02b6b237a
zfnmxt <zfnmxt@zfnmxt.com>
2025-11-04 11:53:12 -0500

    Add the simulator.

--- src/Leak/SecretPC/Sim.hs
+++ src/Leak/SecretPC/Sim.hs
@@ -1,10 +1,28 @@
 {-# LANGUAGE StandaloneDeriving #-}
 
-module Leak.SecretPC.Sim where
+module Leak.SecretPC.Sim
+  ( circuit,
+    init,
+    State,
+  )
+where
 
-import Core
-import Data.Functor.Identity (Identity(Identity))
+import qualified Core
+import Data.Functor.Identity (Identity (Identity))
+import Prelude hiding (init)
+
+type Input = Core.Input Identity
 
 type State = Core.State Identity
 
+type Output = Core.Output Identity
+
+type SimM = Core.CPUM Identity
+
 deriving instance Eq Leak.SecretPC.Sim.State
+
+init :: State
+init = Core.init
+
+circuit :: State -> Input -> (State, Output)
+circuit = Core.circuit
