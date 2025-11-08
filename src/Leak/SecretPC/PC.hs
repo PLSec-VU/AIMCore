c5563554d72cd499b9921986718baf14d9e029da
Wind Wong <t.f.w.wong@vu.nl>
2025-11-08 21:32:40 +0100

    add non interference checks to SecretPC

--- src/Leak/SecretPC/PC.hs
+++ src/Leak/SecretPC/PC.hs
@@ -1,4 +1,16 @@
-module Leak.SecretPC.PC where
+{-# LANGUAGE UndecidableInstances #-}
+
+module Leak.SecretPC.PC
+  ( obs,
+    leak,
+    sim,
+    circuit,
+    proj,
+    implementation,
+    theoryNonInterference0,
+    theoryNonInterference1,
+  )
+where
 
 import Access (PubSec, censor)
 import Control.Monad (guard)
@@ -10,9 +22,39 @@ import Data.Monoid (First (..), getFirst)
 import qualified Leak.SecretPC.Leak as Leak
 import qualified Leak.SecretPC.Sim as Sim
 import Types
+import qualified Pantomime as P
+import qualified Pantomime.Clash as Clash
+import qualified Pantomime.Base as Base
+import Data.Bifunctor (second)
+import Data.Composition
+
+{-# ANN theory (P.Theory $ Base.axioms <> Clash.axioms) #-}
+theory :: Core.State PubSec -> Input PubSec -> Bool
+theory = P.pantomime P.Pantomime
+  { observation = obs'
+  , implementation = implementation
+  , leakage = leak
+  , simulator = sim
+  , projection = proj
+  }
 
 implementation :: Core.State PubSec -> Input PubSec -> (Core.State PubSec, Output PubSec)
 implementation = Core.circuit
+
+circuits :: P.NonInterference (Core.State PubSec) () Sim.State (Input PubSec) Leak.Out (Maybe Address)
+circuits = P.NonInterference
+  { P.implementation = second obs' .: implementation
+  , P.leakage = leak
+  , P.projection = proj
+  }
+
+{-# ANN theoryNonInterference0 (P.Theory $ Base.axioms <> Clash.axioms) #-}
+theoryNonInterference0 :: Core.State PubSec -> Input PubSec -> Bool
+theoryNonInterference0 = P.nonInterference0 circuits
+
+{-# ANN theoryNonInterference1 (P.Theory $ Base.axioms <> Clash.axioms) #-}
+theoryNonInterference1 :: Core.State PubSec -> Input PubSec -> Core.State PubSec -> Input PubSec -> Bool
+theoryNonInterference1 = P.nonInterference1 circuits
 
 stateless :: (a -> b) -> () -> a -> ((), b)
 stateless f _ x = ((), f x)
